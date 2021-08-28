8c47598a958afced5d04fd8bfcec772aa58f40cd
Kishan Kumar <kishan.kumar94173@gmail.com>
2021-08-28 18:04:45 +0530

    completed

--- force-app/main/default/lwc/tableRowComp/tableRowComp.js
+++ force-app/main/default/lwc/tableRowComp/tableRowComp.js
@@ -1,5 +1,34 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement,wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import BOATMC from '@salesforce/messageChannel/QuantityMessageChannel__c';
 
 export default class TableRowComp extends LightningElement {
     @api record;
+    @api check;
+    @api checkk;
+    recId;
+
+    @wire(MessageContext)
+    messageContext;
+
+    connectedCallback(){
+        console.log('in connected callback');
+        this.recId=this.record.Id;
+    }
+    renderedCallback() {
+        console.log('in render'+this.check);
+        if(this.recId!=this.record.Id  ){
+            console.log('updated rec id');
+            this.template.querySelectorAll('lightning-input').forEach(each => { each.checked = this.check; });
+            this.recId=this.record.Id;
+        }
+    }
+    
+    handleChange(event){
+        let isChecked = event.target.checked;
+        const payload = { pId: this.record.Id,isCheckBoxChannel:true,check:isChecked };
+        publish(this.messageContext, BOATMC, payload); 
+
+    }
+
 }
